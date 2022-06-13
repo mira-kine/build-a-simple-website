0a81387e687f7a5c191345fabce30449507832dc
mira-kine <mirak0419@gmail.com>
2022-06-13 13:35:44 -0700

    base editor working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import grapesjs from "grapesjs";
+import React from 'react';
 import './styles/main.scss';
-import Editor from './components/Editor/Editor';
-
+import { useState, useEffect } from 'react';
+import 'grapesjs/dist/css/grapes.min.css';
+import grapesjs from 'grapesjs';
+import gjsPresetWebpage from "grapesjs-preset-webpage"
 // grapesJS is an open-source web builder framework 
 
 function App() {
+  const [editor, setEditor] = useState(null);
+
+    // useEffect to build the editor with grapesJS
+  useEffect(() => {
+    const editor = grapesjs.init({
+      container: "#editor",
+      plugins: [gjsPresetWebpage],
+      pluginsOpts: {
+        gjsPresetWebpage: {}
+      }
+    });
+    setEditor(editor);
+  }, []);
+  // Indicate where to init editor
+
+   
 
   return (
-    <div className="App">\
-      <Editor />
+    <div className="App">
+      <div id="editor"></div>
     </div>
   );
 }
